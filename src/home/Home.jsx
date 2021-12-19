9b01e2e940dfbe8b9a794acbebf8d3a3acfbb637
Galfrevn <galfre.vn@gmail.com>
2021-12-19 16:58:03 -0300

    Featured Homepage

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
 import "./home.scss"
 
@@ -6,11 +7,7 @@ const Home = () => {
     return (
         <div className='home'>
             <Navbar />
-            <img
-                width="100%" 
-                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-                alt="" 
-            />
+            <Featured />
         </div>
     )
 }
