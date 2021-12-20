3ca88a96c6412aa914bc746b1ee2897e7282f418
Galfrevn <galfre.vn@gmail.com>
2021-12-20 14:19:53 -0300

    Watch page and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
 import "./app.scss"
 
 const App = () => {
 
-  return <Home />
+  return <Watch />
 
 };
 
